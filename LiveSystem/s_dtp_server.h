d3e32bb77ad51e8eaa5cfe77f103216e0d4824e0
simonkorl <machuan0228@sina.com>
2022-10-05 20:53:44 +0800

    hack: display demo server

--- LiveSystem/s_dtp_server.h
+++ LiveSystem/s_dtp_server.h
@@ -675,7 +675,7 @@ int dtp_server(const char *host, const char *port, const char *conf) {
     quiche_config_set_initial_max_stream_data_bidi_remote(config, 1000000000);
     quiche_config_set_initial_max_streams_bidi(config, 1000000);
     quiche_config_set_cc_algorithm(config, QUICHE_CC_CUBIC);
-    quiche_config_set_redundancy_rate(config, 1.0f); //
+    quiche_config_set_redundancy_rate(config, 0.5f); //
 
     struct connections c;
     c.sock = sock;
